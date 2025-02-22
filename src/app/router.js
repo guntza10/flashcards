0e81518ee97fe61f2b74f71d2fafa8c63bd2adf1
guntza10 <jomphop.saibuatong@hotmail.com>
2025-02-22 15:59:07 +0700

    refactor: change to use router with object instead jsx way

---
+++ src/app/router.js
@@ -0,0 +1,24 @@
+import AppLayout from "./AppLayout"
+import NewQuizForm from "../components/NewQuizForm"
+import NewTopicForm from "../components/NewTopicForm"
+import Topics from "../features/topics/Topics"
+import Topic from "../features/topics/Topic"
+import Quiz from "../features/quizzes/Quiz"
+import Quizzes from "../features/quizzes/Quizzes"
+
+const routes = [
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "topics", element: <Topics /> },
+      { path: "topics/new", element: <NewTopicForm /> },
+      { path: "topics/:topicId", element: <Topic /> },
+      { path: "quizzes", element: <Quizzes /> },
+      { path: "quizzes/new", element: <NewQuizForm /> },
+      { path: "quizzes/:quizId", element: <Quiz /> },
+    ],
+  },
+]
+
+export default routes
