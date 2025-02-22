abb47c78536b849ac3a1a3234ee255900ad5da72
guntza10 <jomphop.saibuatong@hotmail.com>
2025-02-22 17:37:44 +0700

    refactor(quiz): get data from store's state instead

--- src/features/quizzes/Quiz.js
+++ src/features/quizzes/Quiz.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link, useParams, Navigate } from "react-router-dom";
-import Card from "../cards/Card";
-import ROUTES from "../../app/routes";
-// import quiz selector
+import React from "react"
+import { Link, useParams, Navigate } from "react-router-dom"
+import Card from "../cards/Card"
+import ROUTES from "../../app/routes"
+
+import { useSelector } from "react-redux"
+import { selectQuizzes } from "./quizzesSlice"
 
 export default function Quiz() {
-  const quizzes = {}; // replace this with a call to your selector to get all the quizzes in state
-  const { quizId } = useParams();
-  const quiz = quizzes[quizId];
+  const quizzes = useSelector(selectQuizzes)
+  const { quizId } = useParams()
+  const quiz = quizzes[quizId]
 
-  if(!quiz) {
-    return <Navigate to={ROUTES.quizzesRoute()} replace/>
+  if (!quiz) {
+    return <Navigate to={ROUTES.quizzesRoute()} replace />
   }
-
 
   return (
     <section>
@@ -27,5 +27,5 @@ export default function Quiz() {
         Create a New Quiz
       </Link>
     </section>
-  );
+  )
 }
