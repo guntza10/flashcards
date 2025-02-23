ce15b922e20814243b3304217bcc94c820f8012d
guntza10 <jomphop.saibuatong@hotmail.com>
2025-02-23 14:34:47 +0700

    fix(topicsSlice): fix bug topics should be object instead array

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit"
 const topicsSlice = createSlice({
   name: "topics",
   initialState: {
-    topics: [],
+    topics: {},
   },
   reducers: {
     addTopic: (state, action) => {
